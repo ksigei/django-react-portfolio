219d6846d7b848b327c8b15efdc1bff4beb48171
ksigei <sigeikiprono4@gmail.com>
2023-09-19 13:01:08 +0300

    react

--- api/config/urls.py
+++ api/config/urls.py
@@ -2,11 +2,11 @@ from django.contrib import admin
 from django.urls import path, include
 from rest_framework_swagger.views import get_swagger_view
 
-schema_view = get_swagger_view(title='API Documentation')
+# schema_view = get_swagger_view(title='API Documentation')
 
 urlpatterns = [
     path('admin/', admin.site.urls),
-    path('api/', schema_view),
+    # path('api/', schema_view),
     path('api/', include('blog.urls')),
     path('api/', include('home.urls')),
     path('api/', include('shop.urls')),
